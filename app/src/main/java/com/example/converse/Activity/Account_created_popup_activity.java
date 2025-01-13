3044346b3485088977b92cc976b882ddd4e3864a
SarthakJakhmola <sarthakjakhmola21@gmail.com>
2025-01-13 22:05:27 +0530

    This is the third commit.

---
+++ app/src/main/java/com/example/converse/Activity/Account_created_popup_activity.java
@@ -0,0 +1,33 @@
+package com.example.converse.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.converse.R;
+
+public class Account_created_popup_activity extends AppCompatActivity {
+Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_created_popup);
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainPage.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
